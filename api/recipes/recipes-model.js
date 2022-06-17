795b7d65456fb433d0960526ff8094d50f3a69b4
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-16 19:54:30 -0700

    Worked on database access function

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,5 +1,17 @@
-function getRecipeById(recipe_id) {
+const database = require('../../data/db-config');
 
+async function getRecipeById(recipe_id) {
+    const recipeRow = await database('recipes').where('recipe_id', recipe_id).first();
+
+    const recipe = {
+        recipe_id: recipeRow.recipe_id,
+        recipe_name: recipeRow.recipe_name,
+        created_at: Date.now().toLocaleString(),
+
+
+    };
+
+    return recipe;
 }
 
 module.exports = { getRecipeById };
